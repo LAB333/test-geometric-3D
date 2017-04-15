1ae452a7a0b7b87e36c018d840b3fefff1b87f67
Yorick <yorick.demichelis@defenseurdesdroits.fr>
2017-04-15 18:45:39 +0200

    roomservice

--- functions.js
+++ functions.js
@@ -1,80 +1,53 @@
 var gl;
 
-function loadShader(url, callback){
-    var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function() {
-        if(xhr.readyState == XMLHttpRequest.DONE) {
-            var response = xhr.responseText;
-            callback(response);
-        }
-    };
-    xhr.open("GET", url, true);
-    xhr.responseType = "text";
-    xhr.send();
-}
-
 function initGL(canvas) {
-    try {
-        gl = canvas.getContext("experimental-webgl");
-        gl.viewportWidth = canvas.width;
-        gl.viewportHeight = canvas.height;
-    } catch (e) {
-    }
-    if (!gl) {
-        alert("Could not initialise WebGL, sorry :-(");
-    }
+  try {
+    gl = canvas.getContext("experimental-webgl");
+    gl.viewportWidth = canvas.width;
+    gl.viewportHeight = canvas.height;
+  } catch (e) {
+  }
+  if (!gl) {
+    alert("Could not initialise WebGL, sorry :-(");
+  }
 }
 
-
-function getShader(gl, id) {
-    var shaderScript = document.getElementById(id);
-    if (!shaderScript) {
-        return null;
+function loadShader(url, callback){
+  var xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function() {
+    if(xhr.readyState == XMLHttpRequest.DONE) {
+      var response = xhr.responseText;
+      callback(response);
     }
-
-    var str = "";
-    var k = shaderScript.firstChild;
-    while (k) {
-        if (k.nodeType == 3) {
-            str += k.textContent;
-        }
-        k = k.nextSibling;
-    }
-
-    var shader;
-    if (shaderScript.type == "x-shader/x-fragment") {
-        shader = gl.createShader(gl.FRAGMENT_SHADER);
-    } else if (shaderScript.type == "x-shader/x-vertex") {
-        shader = gl.createShader(gl.VERTEX_SHADER);
-    } else {
-        return null;
-    }
-
-    gl.shaderSource(shader, str);
-    gl.compileShader(shader);
-
-    if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
-        alert(gl.getShaderInfoLog(shader));
-        return null;
-    }
-
-    return shader;
+  };
+  xhr.open("GET", url, true);
+  xhr.responseType = "text";
+  xhr.send();
 }
-
-
-var shaderProgram;
 
 function initShaders(vertexSource, fragmentSource){
-  var fragmentShader = getShader(gl, "shader-fs");
-  var vertexShader = getShader(gl, "shader-vs");
+  var vertexShader, fragmentShader;
+  vertexShader = gl.createShader(gl.VERTEX_SHADER);
+  fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
+
+  gl.shaderSource(vertexShader, vertexSource);
+  gl.shaderSource(fragmentShader, fragmentSource);
+
+  gl.compileShader(vertexShader);
+  gl.compileShader(fragmentShader);
 
   shaderProgram = gl.createProgram();
   gl.attachShader(shaderProgram, vertexShader);
   gl.attachShader(shaderProgram, fragmentShader);
   gl.linkProgram(shaderProgram);
 
-  if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
-      alert("Could not initialise shaders");
+  if(!gl.getShaderParameter(vertexShader, gl.COMPILE_STATUS)){
+    var info = gl.getShaderInfoLog(vertexShader);
+    throw 'Could not compile WebGL program. \n\n' + info;
+  }
+  if(!gl.getShaderParameter(fragmentShader, gl.COMPILE_STATUS)){
+    var info = gl.getShaderInfoLog(fragmentShader);
+    throw 'Could not compile WebGL program. \n\n' + info;
   }
  
 }
@@ -94,18 +67,14 @@ function initAttributesAndUniforms(attributesNames, uniformsNames){
   shaderProgram.mvMatrixUniform = gl.getUniformLocation(shaderProgram, "uMVMatrix");
 }
 
-
-
-
-
 function setMatrixUniforms(pMatrix, mvMatrix) {
-    gl.uniformMatrix4fv(shaderProgram.pMatrixUniform, false, pMatrix);
-    gl.uniformMatrix4fv(shaderProgram.mvMatrixUniform, false, mvMatrix);
+  gl.uniformMatrix4fv(shaderProgram.pMatrixUniform, false, pMatrix);
+  gl.uniformMatrix4fv(shaderProgram.mvMatrixUniform, false, mvMatrix);
 }
 
 
 function degToRad(degrees) {
-    return degrees * Math.PI / 180;
+  return degrees * Math.PI / 180;
 }
 
 
@@ -118,35 +87,32 @@ function collectSecenes(){
 var lastTime = 0;
 
 function animate() {
-    var timeNow = new Date().getTime();
-    if (lastTime != 0) {
-        var elapsed = timeNow - lastTime;
-        scenes[currentScene].animate(elapsed);
-    }
-    lastTime = timeNow;
+  var timeNow = new Date().getTime();
+  if (lastTime != 0) {
+    var elapsed = timeNow - lastTime;
+    scenes[currentScene].animate(elapsed);
+  }
+  lastTime = timeNow;
 }
 
 
 function tick() {
-    requestAnimFrame(tick);
-    scenes[currentScene].drawScene();
-    animate();
+  requestAnimFrame(tick);
+  scenes[currentScene].drawScene();
+  animate();
 }
 
 
 
 function webGLStart() {
+  collectSecenes();
+  var canvas = document.querySelector('canvas');
 
-    collectSecenes();
-    var canvas = document.querySelector('canvas');
+  initGL(canvas);
+  scenes[currentScene].initBuffers(); //TODO this should be called once per scene
 
-    initGL(canvas);
-    initShaders(); //TODO this should be called once per scene
-    scenes[currentScene].initBuffers(); //TODO this should be called once per scene
+  gl.clearColor(0.0, 0.0, 0.0, 1.0);
+  gl.enable(gl.DEPTH_TEST);
 
-    gl.clearColor(0.0, 0.0, 0.0, 1.0);
-    gl.enable(gl.DEPTH_TEST);
-
-    tick();
-
+  tick();
 }
